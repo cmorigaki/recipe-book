7c844f86acf890d7fc99196fa29643b66abf6bb0
Cesar Morigaki <cesar.morigaki@gmail.com>
2020-03-29 12:23:16 -0300

    First commit

---
+++ app/src/main/java/br/com/recipebook/RecipeCollectionActivity.kt
@@ -0,0 +1,11 @@
+package br.com.recipebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeCollectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recipe_collection_activity)
+    }
+}
