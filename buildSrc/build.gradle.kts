07cda7be41f2016fffb67fa884a894f4265f77a0
cmorigaki <cesar.morigaki@gmail.com>
2020-05-17 14:15:35 -0300

    Reverting to groovy gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,36 +1,11 @@
-import org.gradle.kotlin.dsl.`kotlin-dsl`
+repositories {
+    jcenter()
+}
 
 plugins {
     `kotlin-dsl`
-    `kotlin-dsl-precompiled-script-plugins`
 }
 
-buildscript {
-
-    repositories {
-        google()
-        jcenter()
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
-    }
-}
-
-
-repositories {
-    jcenter()
-    google()
-}
-
-val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    languageVersion = "1.3"
-}
-
-dependencies {
-    implementation("com.android.tools.build:gradle:4.0.0-rc01")
-    implementation("com.android.tools.build:gradle-api:4.0.0-rc01")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.72")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+kotlinDslPluginOptions {
+    experimentalWarning.set(false)
 }
