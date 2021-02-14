58f93249828582c49f57c355a3a7d1053ec49c23
cmorigaki <cesar.morigaki@gmail.com>
2021-02-14 11:17:28 -0300

    Using CDN provider instead of S3

--- core-android/src/main/java/br/com/recipebook/coreandroid/configuration/Configuration.kt
+++ core-android/src/main/java/br/com/recipebook/coreandroid/configuration/Configuration.kt
@@ -1,5 +1,5 @@
 package br.com.recipebook.coreandroid.configuration
 
 object Configuration {
-    const val IMG_URL = "https://cm-recipe-book.s3-sa-east-1.amazonaws.com/images/"
+    const val IMG_URL = "https://d20y9ud0nwoqf1.cloudfront.net/images/"
 }
