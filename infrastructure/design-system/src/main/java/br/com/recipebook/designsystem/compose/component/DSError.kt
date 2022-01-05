1f9b0f71c786dd6ee327c9f91e470fd686084c67
cmorigaki <cesar.morigaki@gmail.com>
2022-01-05 13:31:38 -0300

    Removing unneded dependencies

--- infrastructure/design-system/src/main/java/br/com/recipebook/designsystem/compose/component/DSError.kt
+++ infrastructure/design-system/src/main/java/br/com/recipebook/designsystem/compose/component/DSError.kt
@@ -1,17 +1,35 @@
 package br.com.recipebook.designsystem.compose.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import br.com.recipebook.designsystem.R
+import br.com.recipebook.designsystem.compose.Spacing
 
 @Composable
 fun DSError(modifier: Modifier = Modifier) {
-    Image(
-        modifier = modifier,
-        painter = painterResource(id = R.drawable.recipe_error_state),
-        contentDescription = stringResource(id = R.string.error),
-    )
+    Column(modifier = modifier.padding(Spacing.MarginLarge100)) {
+        Image(
+            modifier = modifier.align(Alignment.CenterHorizontally),
+            painter = painterResource(id = R.drawable.recipe_error_state),
+            contentDescription = stringResource(id = R.string.error),
+        )
+        Text(
+            text = stringResource(id = R.string.error_state_generic_title),
+            modifier = modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.h6,
+        )
+        Text(
+            text = stringResource(id = R.string.error_state_generic_subtitle),
+            modifier = modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.subtitle1,
+        )
+    }
 }
--- infrastructure/image/src/main/java/br/com/recipebook/image/ImageLibraryJob.kt
+++ infrastructure/image/src/main/java/br/com/recipebook/image/ImageLibraryJob.kt
@@ -2,10 +2,9 @@ package br.com.recipebook.image
 
 import android.app.Application
 import br.com.recipebook.startup.StartupJob
-import com.facebook.drawee.backends.pipeline.Fresco
 
 class ImageLibraryJob(private val application: Application) : StartupJob {
     override suspend fun invoke() {
-        Fresco.initialize(application)
+        // FIXME Unused after compose migration
     }
 }
